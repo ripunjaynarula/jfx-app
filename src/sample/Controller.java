f2e5828c6ec7852486822f7ce492d8f78e154f8a
ripunjaynarula <ripunjay.narula2019@vitstudent.ac.in>
2020-08-25 23:02:53 +0530

    second commit

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+
 public class Controller {
+    @FXML private TextField newtask;
+    public void addnewtask(ActionEvent e){
+
+    }
+    public void exitprogram(ActionEvent e){
+
+        System.exit(0);
+    }
 }
