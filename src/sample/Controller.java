ef72db688aaba3af0c18b85163ca4e432cd70971
ripunjaynarula <ripunjay.narula2019@vitstudent.ac.in>
2020-08-26 02:15:39 +0530

    course done

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,16 +2,21 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.net.URL;
 import java.util.List;
+import java.util.ResourceBundle;
+import java.util.Scanner;
 
 
-public class Controller {
+public class Controller implements Initializable {
     @FXML private TextField newtask;
     @FXML private ListView<String> tasklist;
     private String filepath="tasks.txt";
@@ -52,6 +57,22 @@ public class Controller {
             tasklist.getItems().remove(selected);
         } else {
             System.out.println("NO SELECTION MADE");
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            Scanner myReader= new Scanner(data);
+
+            while (myReader.hasNextLine()){
+                String text=myReader.nextLine();
+                tasklist.getItems().add(text);
+
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
     }
 }
